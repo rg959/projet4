84fe4d2b2ae4e685a76ee2bf0d64a5b1e15810a7
Rayan GALOUZI <48186600+rg959@users.noreply.github.com>
2020-08-16 20:51:35 +0200

    Add files via upload

---
+++ app/connect.php
@@ -0,0 +1,37 @@
+<?php
+$username = filter_input(INPUT_POST, 'username');
+$password = filter_input(INPUT_POST, 'password');
+if (!empty($username)){
+if (!empty($password)){
+$host = "https://51.83.133.1:3000/";
+$dbUsername = "phpmyadmin";
+$dbPassword = "rWV$mj7ahB";
+$dbname = "phpmyadmin";
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+if (mysqli_connect_error()){
+die('Connect Error ('. mysqli_connect_errno() .') '
+. mysqli_connect_error());
+}
+else{
+$sql = "SELECT username, password FROM admin_user WHERE username='$username' && password='$password'";
+if ($conn->query($sql)){
+echo "New record is inserted sucessfully";
+}
+else{
+echo "Error: ". $sql ."
+". $conn->error;
+}
+$conn->close();
+}
+}
+else{
+echo "Password should not be empty";
+die();
+}
+}
+else{
+echo "Username should not be empty";
+die();
+}
+?>
